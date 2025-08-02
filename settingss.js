f2414b7b7746ce1a020401d1b2a275950095d3ea
dana123868 <dananjayadilshan006@gmail.com>
2025-08-02 15:37:50 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~J7EkTYCb#JFTxAaxiLkMh773ZZWQlJVZmgh5iX8TGVBfy50dVvSc",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "94729596142", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
